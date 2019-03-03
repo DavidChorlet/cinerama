681b46b20fda93bc0bcc5cfca55335956c84d531
DavidChorlet <davidchorlet@gmail.com>
2019-03-03 16:21:39 +0100

    htacess

---
+++ views/error-404.php
@@ -0,0 +1,9 @@
+<?php
+include 'header.php';
+?>
+
+<div class="card"><img src="../assets/pictures/404.png" /></div>
+
+<?php
+include 'footer.php';
+        ?>
---
+++ views/maintenance.php
@@ -0,0 +1,7 @@
+<?php
+include 'header.php';
+?>
+<div class="card"><img src="../assets/pictures/maintenance" /></div>
+<?php
+include 'footer.php';
+?>
